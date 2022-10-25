339232aa2c3508dd388c0c9a63702160a499c653
Van Edelman (RIT Student) <vae3742@rit.edu>
2022-10-25 18:17:08 -0400

    added some curly braces to make code more clear

--- src/main-js.js
+++ src/main-js.js
@@ -36,15 +36,18 @@ const x = beside(plainTile('red'), plainTile('green'));
 // generate an svg string from a tile
 const tileToSvg = (tile) => {
     switch (tile._tag) {
-        case 'Plain':
+        case 'Plain':{
             const {color} = tile;
             return `<rect x="0" y="0" width="1" height="1" fill="${color}" />`;
-        case 'fliph':
+        }
+        case 'fliph':{
             const {tile} = tile;
             return `<g transform="scale(-1, 1) translate(-1, 0)">${tileToSvg(tile)}</g>`;
-        case 'clockwise':
+        }
+        case 'clockwise':{
             const {tile: tileToRotate} = tile;
             return `<g transform="rotate(90) translate(0, -1)">${tileToSvg(tileToRotate)}</g>`;
+        }
     }
 };
 
